c54e405424c95ed200d616aab3d812037501e129
Reyansh-Manta <mantareyansh@gmail.com>
2025-07-30 05:03:52 +0530

    yeahh

--- frontend/myrohrufrontend/app/page.jsx
+++ frontend/myrohrufrontend/app/page.jsx
@@ -1,18 +1,47 @@
 "use client"
 
+import { useRouter } from "next/navigation"
+import axios from "axios"
+import { useEffect } from "react"
+import styles from "./page.module.css"
+
 export default function HomePage() {
-return(
-    <>
-    <div className="toplogo">
-        <div className="logo">
-            <img src="/My.png" alt="" />
-        </div>
-    </div>
-    <div>
-        <div className="weatbox">
+
+    const router = useRouter()
+
+    useEffect(() => {
+        async function check() {
+            try {
+                console.log('fjr');
+
+                await axios.get('http://localhost:2001/api/v1/users/current-user', {
+                    withCredentials: true
+                })
+            }
+            catch (error) {
+            router.push('/unprotected-home')
+        }
             
-        </div>
-    </div>
-    </>
-)
+        }
+        
+        check()
+    }, [router])
+
+    return (
+        <>
+            <div className={styles.toplogo}>
+                <div className={styles.logo}>
+                    <img src="/My.png" alt="" />
+                </div>
+                <div className={styles.navr}>
+                    <a href="/dashboard"><h3>Dashboard</h3></a>
+                </div>
+            </div>
+            <div>
+                <div className="weatbox">
+
+                </div>
+            </div>
+        </>
+    )
 }
