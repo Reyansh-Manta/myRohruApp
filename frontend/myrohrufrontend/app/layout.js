38f22b153dd9258cecd768d2ac40cc96d6b27b33
Reyansh-Manta <mantareyansh@gmail.com>
2025-10-03 19:34:45 +0530

    add HTMlL title

--- frontend/myrohrufrontend/app/layout.js
+++ frontend/myrohrufrontend/app/layout.js
@@ -4,6 +4,14 @@ import './globals.css';
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>My Rohru</title>
+        description="A community platform for Rohru, Himachal Pradesh"
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Bungee&family=Caveat+Brush&display=swap" rel="stylesheet" />
+      </head>
       <body>
         <UserProvider>
           {children}
