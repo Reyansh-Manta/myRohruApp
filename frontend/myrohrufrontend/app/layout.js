6f99689967ccef2df2615324e6f9c91dc888f83d
Reyansh-Manta <mantareyansh@gmail.com>
2025-10-03 19:39:26 +0530

    add HTML description

--- frontend/myrohrufrontend/app/layout.js
+++ frontend/myrohrufrontend/app/layout.js
@@ -6,8 +6,11 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <title>My Rohru</title>
-        description="A community platform for Rohru, Himachal Pradesh"
+        <meta name="description" content="A community platform for Rohru, Himachal Pradesh" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Bungee&family=Caveat+Brush&display=swap" rel="stylesheet" />
       </head>
       <body>
         <UserProvider>
